9bfabcc17bd6cbe8b65ab32989013fb06a3532c3
NAHWorks <DaemonRonin@proton.me>
2026-02-08 23:48:21 -0500

    Add parser arguments

--- Netcat_Replace.py
+++ Netcat_Replace.py
@@ -26,4 +26,17 @@ if __name__ == '__main__':
             echo 'ABCDEFGHI' | ./netcat.py -t 192.168.1.108 -p 135 # echo local text to server port 135
             netcat.py -t 192.168.1.108 -p 5555 # connect to server
         '''))
-      
+    parser.add_argument('-c', '--command', action='store_true', help='initialize command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+
+    nc = NetCat(args, buffer.encode('utf-8'))
+    nc.run() 
